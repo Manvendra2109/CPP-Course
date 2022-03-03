b96694e741d0b87334f98811dc45874389d2a447
Manvendra2109 <bondsingh@gmail.com>
2022-03-04 00:55:35 +0530

    forE

---
+++ Arrays/for_eachloop.cpp
@@ -0,0 +1,11 @@
+// for each loop is used in array or collection type of data structure In the syntax of foreach loop (x) is a declareing variable for elements in Array //
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int A[] = {3,5,6,7,8,9,10};
+    for (int x:A)
+    cout << x << endl;
+    return 0;
+}
