aec54725676854ea80d49fbb6c94fb06918aa423
Manvendra2109 <bondsingh@gmail.com>
2022-03-06 16:21:26 +0530

    add

---
+++ Arrays/add_array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int A[7]={ 9,8,7,6,5,4,3},sum=0;
+    for ( int i=0; i<7; i++ )
+    {
+        sum = sum + A[i];
+        
+    } cout <<" sum is :" << sum   << endl;
+    return 0;
+
+
+
+}
