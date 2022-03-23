17ad234b87bc87e53a0d41df3e13b2d8db04fac3
Manvendra2109 <manvendra.20scse1010358@galgotiasuniversity.edu.in>
2022-03-23 22:07:43 +0530

    address

--- Pointers/dec.cpp
+++ Pointers/dec.cpp
@@ -1,3 +1,4 @@
+// in this the 0x61ff0c 0x61ff08 0x61ff0c these are the addresses of the predefined variable its not the error //
 #include<iostream>
 using namespace std;
 int main(){
@@ -5,8 +6,10 @@ int main(){
     int *p=&a;
     cout << a <<endl;
     cout << &a << endl;
-    cout << p<< endl;
-    cout << *p<< endl;
+    cout << &p << endl;
+    cout << p << endl;
+    cout << *p << endl;
+     
 
 return 0;
 }
