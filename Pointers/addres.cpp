f769e952795a0a8e3a63663bbf5ac9009212e6c6
Manvendra2109 <manvendra.20scse1010358@galgotiasuniversity.edu.in>
2022-04-04 23:17:58 +0530

    poin

---
+++ Pointers/addres.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A=33;
+    int B=43;
+    int C=323;
+    cout << "address of A :" << &A << endl;
+    cout << "add. of B :"  << &B << endl;
+    cout << "ad of C :" << &C << endl;
+    return 0;
+}
