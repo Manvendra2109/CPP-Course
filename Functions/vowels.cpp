5b26f78fec29e1eedd3fd667bc1c799eb4437ee7
Manvendra2109 <manvendra.20scse1010358@galgotiasuniversity.edu.in>
2022-04-11 19:03:47 +0530

    o

--- Functions/vowels.cpp
+++ Functions/vowels.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
-char vowels ()
+char vowels (char step)
 {
-    
+    if(step=='a'||step=='e'||step=='i'||step=='o'||step=='u')
+    return 'v';
+    else 
+    return 'c';
+
+}
+string vowels2 ( char step  )
+{   
+     if(step=='a'||step=='e'||step=='i'||step=='o'||step=='u')
+    return "vowels";
+    else 
+    return "consonants";
+
+}
+int main ()
+{
+    char step;
+    string r;
+    cout << "step" << endl;
+    cin >> step;
+    r=vowels2(step);
+    cout << "answer is :"  << r << endl;
+    return 0;
 }
