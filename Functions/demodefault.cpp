8400d12356c58689eb87c191ba28153bd732012c
Manvendra2109 <manvendra.20scse1010358@galgotiasuniversity.edu.in>
2022-04-18 00:14:27 +0530

    new

---
+++ Functions/demodefault.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void swap ( int x, int y)
+{
+    cout << x << " " << y << endl;
+    int temp;
+    temp =x;
+    x=y;
+    y=temp;
+    cout << x << " " << y << endl;
+
+}
+int main ()
+{
+    int x=10;
+    int y=20;
+    swap(x,y);
+    cout << x << " " << y<< endl;
+    return 0;
+}
