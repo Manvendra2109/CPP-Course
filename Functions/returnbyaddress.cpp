4b33f2a6faad04a136e652e93936fe62dcc50434
Manvendra2109 <manvendra.20scse1010358@galgotiasuniversity.edu.in>
2022-04-28 01:27:04 +0530

    sahi

---
+++ Functions/returnbyaddress.cpp
@@ -0,0 +1,20 @@
+// isi me agar address print krana h to uske liye bs coutt << p << likhna hog return p ke upar aur *q dena padega
+#include <iostream>
+using namespace std;
+int * func()
+{
+    int*p=new int[5];
+    for(int i=0; i<5; i++)
+    {
+        p[i]=5*i;
+    }
+    return p;
+}
+int main ()
+{
+    int*q=func();
+    cout << q << endl;
+    for(int i=0; i<5; i++)
+    cout << q[i] << endl;
+
+}
