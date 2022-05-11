aa7432fac7a7458922fc461637bba3054f380a6b
Manvendra2109 <manvendra.20scse1010358@galgotiasuniversity.edu.in>
2022-05-11 15:22:37 +0530

    new

---
+++ Functions/addusefunc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int add(int a, int b)
+{
+    return a+b;
+}
+int main ()
+{
+    int sum;
+    sum =add(100 ,75);
+    cout << sum << endl;
+    return 0;
+
+}
