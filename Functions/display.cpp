7fbe2ce20e7ed8f99996ba5fcc6c267043eb731e
Manvendra2109 <manvendra.20scse1010358@galgotiasuniversity.edu.in>
2022-05-13 00:51:38 +0530

    change

--- Functions/display.cpp
+++ Functions/display.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
-void displaynum (int n1, float n2)
+void displaynum (int n1, float n2 , char n3)
 {
     cout << "the int number is :" << n1 << endl;
     cout << "the double number is :" << n2 << endl;
+    cout << "the character is :" << n3 << endl;
 }
-int main ()
+ main ()
 {
     int num1 =5;
     double num2=5.5;
-    displaynum(num1,num2);
+    char num3=55;
+    displaynum(num1,num2,num3);
     return 0;
 }
