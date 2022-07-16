fe50d2eb83dd680dfd9df4078b082a0d384b58e6
Manvendra2109 <manvendra.20scse1010358@galgotiasuniversity.edu.in>
2022-07-16 20:13:32 +0530

    minuswalahogaya

---
+++ Basic Programs/minus.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int main ()
+{
+    int i,n,n1,n2,minus;
+    cout << "enter the two values:" << endl;
+    cin >> n2 >> n1;
+    for(i=0; i<=n; i++)
+    {
+        minus=n2-n1;
+    }
+    cout << "the answer is " << minus << endl;
+    return 0;
+
+}
---
+++ OOPS/constructorinher.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+    public:
+    base()
+    {
+        cout << "Non-parametere value" << endl;
+
+    }
+
+};
+int main()
+{
+    
+}
